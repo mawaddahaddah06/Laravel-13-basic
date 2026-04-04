c4847fae49b69af55219736ff3ddf31a82abcfcf
mawaddah <mawaddah24@mhs.unitama.ac.adgit config --global user.email mawaddah@mhs.unitama.ac.id>
2026-04-04 19:15:26 +0700

    pertemuan 2: Blade Templet

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/Student', function () {
+    return view('student.index', ['title' => 'STUDENT']);
+});
+
+
+
+
+Route::get('/Student/create', function () {
+    return view('student.create', ['title' => 'CREATE STUDENT']);
+});
