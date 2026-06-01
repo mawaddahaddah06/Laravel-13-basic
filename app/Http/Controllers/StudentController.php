35eaee5c576f8bbcf67da5880659e90e8cc12996
mawaddah <mawaddah24@mhs.unitama.ac.adgit config --global user.email mawaddah@mhs.unitama.ac.id>
2026-06-01 18:00:34 +0700

    pertemuan 4: CRUD

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,10 +12,10 @@ class StudentController extends Controller
      */
     public function index()
     {
-         return view('student.index', [
-            'title' => 'STUDENT',
-            'student' => Student::all(), 
-
+        return view('student.index', [
+            'title' => 'Student',
+            //'students' => Student::latest()->get(),
+            'students' => Student::orderBy('name', 'asc')->get(),
             ]);
     }
 
@@ -24,16 +24,29 @@ class StudentController extends Controller
      */
     public function create()
     {
-        return view('student.create', ['title' => 'CREATE STUDENT']);
+        return view('student.create', ['title' => 'Create Student']);
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
+{
+    $validated = $request->validate([
+        'name' => 'required|max:255',
+        'nim' =>'required|digits:11|numeric',
+    ],[
+        'name.required'=> 'Nama tidak boleh kosong',
+        'name.max'=> 'Nama tidak boleh lebih dari :max karakter',
+        'nim.required'=> 'NIM tidak boleh kosong',
+        'nim.digits'=> 'NIM wajib :digits',
+        'nim.numeric' => 'Nim wajib angka',
+    ]);
+
+    Student::create($validated);
+
+    return to_route('student.index')->withSuccess('Data Berhasil Ditambahkan');
+}
 
     /**
      * Display the specified resource.
@@ -48,7 +61,10 @@ class StudentController extends Controller
      */
     public function edit(Student $student)
     {
-        //
+        return view('student.edit', [
+            'title' => 'Edit Student',
+            'student' => $student,
+            ]);
     }
 
     /**
@@ -56,7 +72,20 @@ class StudentController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+        $validated = $request->validate([
+        'name' => 'required|max:255',
+        'nim' => 'required|digits:11|numeric',
+    ], [
+        'name.required' => 'Nama Tidak Boleh Kosong',
+        'name.max' => 'Nama Maksimal 255 Karakter',
+        'nim.required' => 'NIM Tidak Boleh Kosong',
+        'nim.digits' => 'NIM Harus 11 Digit',
+        'nim.numeric' => 'NIM Harus Angka',
+    ]);
+
+    $student->update($validated);
+ 
+    return to_route('student.index')->withSuccess('Data Berhasil Diubah');
     }
 
     /**
@@ -64,6 +93,7 @@ class StudentController extends Controller
      */
     public function destroy(Student $student)
     {
-        //
+        $student->delete($student);
+        return to_route('student.index')->withSuccess('Data Berhasil Dihapus');  
     }
 }
